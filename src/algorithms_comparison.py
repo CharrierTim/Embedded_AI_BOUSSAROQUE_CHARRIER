3a8949f0d16401cb695adb639b1a2be6552d03c2
CharrierTim <timothee.charrier@bbox.fr>
2023-09-14 12:49:10 +0200

    feat: add KNN, RandomForest and NN

--- src/algorithms_comparison.py
+++ src/algorithms_comparison.py
@@ -1,11 +1,33 @@
-from dataset_handling import Dataset
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from dataset_handling import Dataset
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.losses import mean_squared_error
+
 
 def average_error(y_true, y_pred):
+    """
+    Computes the average error between two vectors.
+
+    Attributes:
+    -----------
+    y_true : numpy.ndarray
+        The true values.
+
+    y_pred : numpy.ndarray
+        The predicted values.
+
+     Methods:
+    --------
+    average_error(y_true, y_pred) -> float
+        Computes the average error between two vectors.
+    """
     sum = 0
     for y_t, y_p in zip(y_true, y_pred):
         sum += abs(y_t-y_p)
@@ -33,8 +55,32 @@ Y_pred = log.predict(X_test)
 err = average_error(Y_test, Y_pred)
 print(f'LOG: {err}')
 
-mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(20, 10, 3), random_state=1)
+mlp = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    hidden_layer_sizes=(20, 10, 3), random_state=1)
 mlp.fit(X_train, Y_train)
 Y_pred = mlp.predict(X_test)
 err = average_error(Y_test, Y_pred)
 print(f'MLP: {err}')
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X_train, Y_train)
+Y_pred = rf.predict(X_test)
+err = average_error(Y_test, Y_pred)
+print(f'RF: {err}')
+
+knn = KNeighborsClassifier(n_neighbors=5, weights='distance')
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+err = average_error(Y_test, Y_pred)
+print(f'KNN: {err}')
+
+model = Sequential()
+model.add(Dense(20, input_dim=12, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss=mean_squared_error, optimizer='adam')
+model.fit(X_train, Y_train/10, epochs=15, batch_size=32,
+          validation_data=(X_test, Y_test/10))
+Y_pred = model.predict(X_test)
+err = average_error(Y_test, Y_pred*10)
+print(f'NN: {err}')
