0dc023ca56c5d733131141852791a90a1c468f84
H3Aether <alexis.boussaroque@gmail.com>
2023-09-25 11:12:27 +0200

    feat: added wrapper

--- src/communication_STM32.py
+++ src/communication_STM32.py
@@ -1,16 +1,43 @@
 import serial
+import numpy as np
 
-def synchronise_UART():
-    with serial.Serial("/dev/tty.usbmodem1412403", 115200, timeout=1) as ser: # COM5 for H743 (nucleo) and COM6 for F411 (Nucleo)
-        is_synced = False
-        while (not is_synced):
-            # Send 0xAB
-            ser.write(b"\xAB")
-            ret = ser.read(1)
+PORT = "COM4"
+
+def synchronise_UART(serial_port):
+        while (1):
+            serial_port.write(b"\xAB")
+            ret = serial_port.read(1)
             if (ret == b"\xCD"):
-                is_synced = True
-                print("Synchronised")
+                break
+
+def send_inputs_to_STM32(inputs, serial_port):
+    inputs = inputs.astype(np.float16) # Convert inputs to float16
+    for x in inputs:
+        # serial_port.write(x)
+        pass
+
+def evaluate_model_on_STM32(iterations, serial_port):
+    error = 0
+    for i in range(iterations):
+        print(f"----- Iteration {i} -----")
+        send_inputs_to_STM32(X_test[i], serial_port)
+        # Read output
+        output = 5
+        error += abs(Y_test[i]-output) / iterations
+        print(f"   Expected output: {Y_test[i]}")
+        print(f"   Received output: {output}")
+        print(f"----------------------- Error: {error:.2f}\n")
+    return error
+
 
 if __name__ == '__main__':
-    print("Synchronising...")
-    synchronise_UART()
+    X_test, Y_test = np.load("./dataset/test_X.npy"), np.load("./dataset/test_Y.npy")
+
+    with serial.Serial(PORT, 115200, timeout=1) as ser: # COM5 for H743 (nucleo) and COM6 for F411 (Nucleo)
+        print("Synchronising...")
+        synchronise_UART(ser)
+        print("Synchronised")
+
+        print("Sending inputs...")
+        send_inputs_to_STM32(X_test[0], ser)
+        print("Inputs sent")
