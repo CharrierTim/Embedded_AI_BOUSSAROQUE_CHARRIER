fa964f3c5c455fbca198326aa87fb10743a2eef3
H3Aether <alexis.boussaroque@gmail.com>
2023-10-02 09:09:35 +0200

    feat: Added output reading

--- src/communication_STM32.py
+++ src/communication_STM32.py
@@ -16,14 +16,17 @@ def send_inputs_to_STM32(inputs, serial_port):
     for x in inputs:
         buffer += x.tobytes()
     serial_port.write(buffer)
+
+def read_output_from_STM32(serial_port):
+    output = serial_port.read(1)
+    return output
     
 def evaluate_model_on_STM32(iterations, serial_port):
     error = 0
     for i in range(iterations):
         print(f"----- Iteration {i+1} -----")
         send_inputs_to_STM32(X_test[i], serial_port)
-        # Read output
-        output = 5
+        output = read_output_from_STM32(serial_port)
         error += abs(Y_test[i]-output) / iterations
         print(f"   Expected output: {Y_test[i]}")
         print(f"   Received output: {output}")
@@ -42,3 +45,6 @@ if __name__ == '__main__':
         print("Sending inputs...")
         send_inputs_to_STM32(X_test[0], ser)
         print("Inputs sent")
+
+        print("Reading output...")
+        print(f"Received: {read_output_from_STM32(ser)}")
