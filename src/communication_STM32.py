c49190c1898d2f81cb1c2e1c4dae43c009264247
CharrierTim <timothee.charrier@bbox.fr>
2023-11-08 08:55:03 +0100

    feat: improve data output reception

--- src/communication_STM32.py
+++ src/communication_STM32.py
@@ -1,50 +1,73 @@
 import serial
+import struct
 import numpy as np
 
 PORT = "COM4"
 
+
 def synchronise_UART(serial_port):
-        while (1):
-            serial_port.write(b"\xAB")
-            ret = serial_port.read(1)
-            if (ret == b"\xCD"):
-                break
+    while (1):
+        serial_port.write(b"\xAB")
+        ret = serial_port.read(1)
+        if (ret == b"\xCD"):
+            break
+
 
 def send_inputs_to_STM32(inputs, serial_port):
-    inputs = inputs.astype(np.float16) # Convert inputs to float16
+    inputs = inputs.astype(np.float32)  # Convert inputs to float16
     buffer = b""
     for x in inputs:
         buffer += x.tobytes()
     serial_port.write(buffer)
 
+
 def read_output_from_STM32(serial_port):
-    output = serial_port.read(2)
-    return np.frombuffer(output, dtype=np.float16)[0]
-    
+    output = serial_port.read(12)
+    print(f"Output: {hex(int.from_bytes(output, 'big'))}")
+
+    # Create an empty list to store the extracted float values
+    float_values = []
+
+    # Iterate through the 12 bytes, splitting them into three 32-bit integers
+    for i in range(0, 12, 4):
+        # Extract 4 bytes
+        four_bytes = output[i:i+4]
+
+        # Reverse the order of the bytes
+        reversed_bytes = four_bytes[::-1]
+
+        # Convert the reversed bytes to a float
+        float_value = struct.unpack('f', reversed_bytes)[0]
+
+        # Add the float value to the list
+        float_values.append(float_value)
+
+    # Print the extracted float values
+    for value in float_values:
+        print(value)
+
+
 def evaluate_model_on_STM32(iterations, serial_port):
     error = 0
     for i in range(iterations):
         print(f"----- Iteration {i+1} -----")
         send_inputs_to_STM32(X_test[i], serial_port)
         output = read_output_from_STM32(serial_port)
-        error += abs(Y_test[i]-output) / iterations
+        error += np.mean(abs(Y_test[i]-output)) / iterations
         print(f"   Expected output: {Y_test[i]}")
         print(f"   Received output: {output}")
-        print(f"----------------------- Error: {error:.2f}\n")
+        print(f"----------------------- Error: {np.mean(error):.2f}\n")
     return error
 
 
 if __name__ == '__main__':
-    X_test, Y_test = np.load("./dataset/test_X.npy"), np.load("./dataset/test_Y.npy")
+    X_test, Y_test = np.load(
+        "./dataset/wine_quality_X_test.npy"), np.load("./dataset/wine_quality_Y_test.npy")
 
-    with serial.Serial(PORT, 115200, timeout=1) as ser: # COM5 for H743 (nucleo) and COM6 for F411 (Nucleo)
+    with serial.Serial(PORT, 115200, timeout=1) as ser:
         print("Synchronising...")
         synchronise_UART(ser)
         print("Synchronised")
 
-        print("Sending inputs...")
-        send_inputs_to_STM32(X_test[0], ser)
-        print("Inputs sent")
-
-        print("Reading output...")
-        print(f"Received: {read_output_from_STM32(ser)}")
+        print("Evaluating model on STM32...")
+        error = evaluate_model_on_STM32(10, ser)
