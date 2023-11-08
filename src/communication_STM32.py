800be2b1a629355ca8b9b7c62d83c8095e6c2194
H3Aether <alexis.boussaroque@gmail.com>
2023-11-08 10:04:07 +0100

    fix: changed read_output_from_STM32 to match new embedded software

--- src/communication_STM32.py
+++ src/communication_STM32.py
@@ -22,47 +22,30 @@ def send_inputs_to_STM32(inputs, serial_port):
 
 
 def read_output_from_STM32(serial_port):
-    output = serial_port.read(12)
-    print(f"Output: {hex(int.from_bytes(output, 'big'))}")
+    output = serial_port.read(3)
+    print(f"Raw output: {output}")
 
-    # Create an empty list to store the extracted float values
-    float_values = []
-
-    # Iterate through the 12 bytes, splitting them into three 32-bit integers
-    for i in range(0, 12, 4):
-        # Extract 4 bytes
-        four_bytes = output[i:i+4]
-
-        # Reverse the order of the bytes
-        reversed_bytes = four_bytes[::-1]
-
-        # Convert the reversed bytes to a float
-        float_value = struct.unpack('f', reversed_bytes)[0]
-
-        # Add the float value to the list
-        float_values.append(float_value)
-
-    # Print the extracted float values
-    for value in float_values:
-        print(value)
+    float_values = [int(out)/255 for out in output]
+    {print(val) for val in float_values}
+    return float_values
 
 
 def evaluate_model_on_STM32(iterations, serial_port):
-    error = 0
+    accuracy = 0
     for i in range(iterations):
         print(f"----- Iteration {i+1} -----")
         send_inputs_to_STM32(X_test[i], serial_port)
         output = read_output_from_STM32(serial_port)
-        error += np.mean(abs(Y_test[i]-output)) / iterations
+        if (np.argmax(output) == np.argmax(Y_test[i])):
+            accuracy += 1 / iterations
         print(f"   Expected output: {Y_test[i]}")
         print(f"   Received output: {output}")
-        print(f"----------------------- Error: {np.mean(error):.2f}\n")
+        print(f"----------------------- Accuracy: {accuracy:.2f}\n")
     return error
 
 
 if __name__ == '__main__':
-    X_test, Y_test = np.load(
-        "./dataset/wine_quality_X_test.npy"), np.load("./dataset/wine_quality_Y_test.npy")
+    X_test, Y_test = np.load("./dataset/wine_quality_X_test.npy"), np.load("./dataset/wine_quality_Y_test.npy")
 
     with serial.Serial(PORT, 115200, timeout=1) as ser:
         print("Synchronising...")
